d99727f9dd5f6628803872448403bba8a16860f5
Mustafa Lokhandwala <mustafa.l.serpentcs@gmail.com>
2019-12-06 15:22:20 +0530

    [IMP] Improve the code for the test File

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'app/data/services/globals.dart';
-import 'app/pages/home.dart';
-import 'app/pages/login.dart';
+import 'package:odoo_client/app.dart';
 
-void main() async {
-  SharedPreferences preferences = await SharedPreferences.getInstance();
-  bool _result =
-      preferences.getString(Globals().loginPrefName) != null ? true : false;
-  Widget _defaultHome = Login();
-  if (_result) {
-    _defaultHome = Home();
-  }
-  runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Odoo Mobile',
-      theme: ThemeData(
-        primarySwatch: Colors.indigo,
-        fontFamily: "Montserrat",
-      ),
-      home: _defaultHome,
-      routes: <String, WidgetBuilder>{
-        '/home': (_) => Home(),
-        '/login': (_) => Login(),
-      },
-    ),
-  );
-}
+void main() => runApp(App());
